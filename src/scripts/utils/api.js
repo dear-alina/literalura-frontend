16f21a1ffcf13ada2e3ded8a666f9838fd24aed5
dear-alina <haku16012@gmail.com>
2026-07-14 10:04:26 -0500

    chore: actualizar url base de la api hacia render

--- src/scripts/utils/api.js
+++ src/scripts/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://literalura-mvmt.onrender.com/api';
 
 /**
  * Cliente API centralizado para interactuar con el backend
